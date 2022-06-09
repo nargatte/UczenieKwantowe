362dbaf644412947981216cb27badccbc23affc2
Grzegorz Krzysiak <krzysiak.grzegorz@gmail.com>
2022-06-09 11:58:57 +0200

    qsimcirq tests

---
+++ qsimcirq/gui.py
@@ -0,0 +1,11 @@
+from tkinter import * 
+from tkinter.ttk import *
+root = Tk() 
+test = Label(text="<message>") 
+test.pack() 
+root.mainloop() 
+
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4])
+plt.ylabel('some numbers')
+plt.show()
